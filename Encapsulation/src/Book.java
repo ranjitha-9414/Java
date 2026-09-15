2b8ca86759fa85a4ee32a2e98ccccd2024ecdf5a
ranjitha-9414 <cranjitha82@gmail.com>
2026-09-15 12:36:42 +0530

    updated

---
+++ Encapsulation/src/Book.java
@@ -0,0 +1,15 @@
+
+public class Book {
+ private int pageNum;
+ 
+ public void setData(int x) {
+	 if (x>0) {	
+		 pageNum = x;
+ 
+	 }
+ }
+ 
+ public int getData() {
+	return pageNum;
+ }
+}
---
+++ Encapsulation/src/BookApp.java
@@ -0,0 +1,13 @@
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Book b1 = new Book();
+		b1.setData(25);
+		System.out.println(b1.getData());
+
+	}
+
+}
