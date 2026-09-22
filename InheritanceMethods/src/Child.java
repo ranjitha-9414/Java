88ecfcc3bb6ec7b05f479bd212ddc37d77b9adc3
ranjitha-9414 <cranjitha82@gmail.com>
2026-09-22 09:45:07 +0530

    updated

---
+++ InheritanceMethods/src/Child.java
@@ -0,0 +1,15 @@
+
+public class Child extends Parent{
+
+	void display1() {
+		System.out.println("Inside Chlid Display1");
+	}
+	
+	void display2() {
+		System.out.println("Inside parent Display2");
+	}
+	
+	void display3() {
+		System.out.println("Inside child Display3");
+	}
+}
---
+++ InheritanceMethods/src/Parent.java
@@ -0,0 +1,11 @@
+
+public class Parent {
+	void display1() {
+	System.out.println("Inside Parent display1");
+	}
+	
+	void display2() {
+		System.out.println("Inside Parent display2");
+	}
+	
+}
